ff66b38466453574716f695533f6f34aecbd4913
chenlh <13692238295@163.com>
2022-10-30 21:35:32 +0800

    complete parsing of command line args composed of three different types of boolean,int,string

--- args/src/test/java/cn/gavin/args/ArgsTest.java
+++ args/src/test/java/cn/gavin/args/ArgsTest.java
@@ -10,10 +10,51 @@ public class ArgsTest {
     //[-l], [-p, 8080], [-d, /usr/logs]
     //{-l:[], -p:[8080], -d:[/usr/logs]}
     // Single Option:
-    // TODO:       - bool -l
-    // TODO:       - int -p 8080
-    // TODO:       - string -d /usr/logs
-    // TODO: multi options: -l -p 8080 -d /user/logs
+    @Test
+    public void should_set_boolean_option_to_true_if_flag_present() {
+        BooleanOption option = Args.parse(BooleanOption.class, "-l");
+
+        assertTrue(option.logging());
+    }
+
+    @Test
+    public void should_set_boolean_option_to_false_if_flag_not_present() {
+        BooleanOption option = Args.parse(BooleanOption.class);
+
+        assertFalse(option.logging());
+    }
+
+    record BooleanOption(@Option("l") boolean logging) {
+    }
+
+    @Test
+    public void should_parse_int_as_option_value() {
+        IntOption option = Args.parse(IntOption.class, "-p", "8080");
+        assertEquals(8080, option.port());
+    }
+
+    record IntOption(@Option("p") int port) {
+    }
+
+    @Test
+    public void should_get_string_as_option_value() {
+        StringOption option = Args.parse(StringOption.class, "-d", "/usr/logs");
+        assertEquals("/usr/logs", option.directory());
+    }
+
+    record StringOption(@Option("d") String directory) {
+    }
+
+    @Test
+    public void should_parse_multi_options() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+    }
+
+    record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
     // sad path:
     // TODO:       - bool -l t / -l t f
     // TODO:       - int -p / -p 8080 8081
@@ -23,15 +64,6 @@ public class ArgsTest {
     // TODO:       - int 0
     // TODO:       - string ""
 
-    @Test
-    @Disabled
-    public void should_example_1() {
-        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging());
-        assertEquals(8080, options.port());
-        assertEquals("/usr/logs", options.directory());
-    }
-
     //-g this is a list -d 1 2 -3 5
     @Test
     @Disabled
@@ -40,9 +72,6 @@ public class ArgsTest {
 
         assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
         assertArrayEquals(new int[]{1, 2, -3, 5}, options.decimals());
-    }
-
-    record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
     }
 
     record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
